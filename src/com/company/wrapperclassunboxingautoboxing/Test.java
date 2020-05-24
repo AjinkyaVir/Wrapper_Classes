dcdb8f8c3881734f8aaaa5a99b54c4eec1d1c3a9
Mr-Ajinkya <virkud.ajinkya2248@gmail.com>
2020-05-24 09:16:47 +0530

    Ajinkya - toString() method example

---
+++ src/com/company/wrapperclassunboxingautoboxing/Test.java
@@ -0,0 +1,15 @@
+package com.company.wrapperclassunboxingautoboxing;
+
+import com.sun.corba.se.impl.legacy.connection.USLPort;
+
+public class Test {
+    public static void main(String[] args) {
+
+        Integer i = 10;
+        System.out.println(i);
+
+        int x = new Integer(100);
+        System.out.println(x);
+
+    }
+}
